e81f61d10c471fbc7056f214f13a3cec32fe40e4
mlukicdesign <mlukicdesign@gmail.com>
2024-01-07 16:13:12 +0800

    Created CMS Fields

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# This repo has moved!
+# Payload CMS Site
 
-This repo has been merged into the [Payload Examples Directory](https://github.com/payloadcms/payload/tree/main/examples). Please refer to the [Custom Server Example](https://github.com/payloadcms/payload/tree/main/examples/custom-server) for all the latest updates.
+## Exploration into Payload CMS and its features using the [Payload CMS Documentation](https://payloadcms.com/docs/introduction/) & Verified Payload CMS boiler plate code.
+
+### Installation
+
+1. Clone the repo
+2. Run `npm install`
+3. Resolve Dependency issues
+4. Configure Environment Variables
+5. Configure MongoDB
+6. Run `npm run dev`
+
+### Dependencies
+
+- [Node.js](https://nodejs.org/en/)
+- [MongoDB](https://www.mongodb.com/)
+- [Payload CMS](https://payloadcms.com/)
+- [React](https://reactjs.org/)
+- [Express](https://expressjs.com/)
+- [Next.js](https://nextjs.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Typescript](https://www.typescriptlang.org/)
+
+
+Visit Payload CMS [Documentation](https://payloadcms.com/docs/introduction/) for more information.
--- collections/Page.ts
+++ collections/Page.ts
@@ -7,6 +7,8 @@ import { CallToAction } from '../blocks/CallToAction/Config';
 import { Type as CallToActionType } from '../blocks/CallToAction/Component';
 import { Content } from '../blocks/Content/Config';
 import { Type as ContentType } from '../blocks/Content/Component';
+import slug from '../fields/slug';
+import meta from '../fields/meta';
 
 
 export type Layout = CallToActionType | ContentType | ImageType
@@ -55,41 +57,8 @@ export const Page: CollectionConfig = {
         Image,
       ],
     },
-    {
-      name: 'meta',
-      label: 'Page Meta',
-      type: 'group',
-      fields: [
-        {
-          name: 'title',
-          label: 'Title',
-          type: 'text',
-        },
-        {
-          name: 'description',
-          label: 'Description',
-          type: 'textarea',
-        },
-        {
-          name: 'keywords',
-          label: 'Keywords',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      name: 'slug',
-      label: 'Page Slug',
-      type: 'text',
-      admin: {
-        position: 'sidebar',
-      },
-      hooks: {
-        beforeValidate: [
-          formatSlug('title'),
-        ],
-      },
-    },
+   meta,
+   slug,
   ],
 };
 
